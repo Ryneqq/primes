23718b7f0c6437f10d1f36c9ce58aa361ab9dcc7
Pawel Rynowiecki <pawel.rynowiecki@anixe.pl>
2018-06-22 23:39:13 +0200

    new functions for tests

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 fn main() {
-    let (range, primes) = ((0, 100000000), 3);
+    // test_generating_primes();
+    test_cracking_key();
 
-    // // let range = (200000, 1100000);
-    // let mut filter = PrimeFilter::new(range.0, range.1, primes);
-    // println!("Start filtering, working on {:?} primes", primes);
-    // let generated = filter.filter();
-    // // println!("Sample: {:?}", generated);
-    // println!("Primes are filtered, found: {:?}", generated.len());
-    // println!("Numbers range: <{:?}, {:?}>", range.0, range.1);
-    // println!("Total nummbers to compute: {:?}", range.1 - range.0);
+    // let primes = first_n_numbers(range.1);
+    // println!("Generated primes: {:?}", primes.len());
+}
 
-    // check_percentage_of_primes(generated.clone());
-    // 1000000000000037
-// 153015847,
-//11746811,
+fn test_generating_primes() {
+    let (range, primes) = ((100000000, 200000000), 200);
+
+    let mut filter = PrimeFilter::new(range.0, primes);
+    println!("Start filtering, working on {:?} primes", primes);
+    let generated = filter.filter(range.1);
+    println!("Primes are filtered, found: {:?}", generated.len());
+    println!("Numbers range: <{:?}, {:?}>", range.0, range.1);
+    println!("Total nummbers to compute: {:?}", range.1 - range.0);
+
+    check_percentage_of_primes(generated.clone());
+}
+
+fn test_cracking_key() {
     // let key: u64 = 11746811 * 1993209007;
-    // // let cracked = crack_classic(key);
-    // // println!("We cracked the key: {:?}, primes: {:?} ", key, cracked);
-
-    // println!("key % 12373 {:?}", key % 12373);
-
-    let primes = first_n_numbers(range.1);
-    println!("Generated primes: {:?}", primes.len());
+    let key: u64 = 1993209007 * 1993209007;
+    println!("Trying to crack the key: {:?}", key);
+    let cracked = crack(key, 100);
+    // let cracked = crack_classic(key);
+    println!("Cracked, primes: {:?} ", cracked);
 }
 
 fn check_percentage_of_primes(prime: Vec<u64>) {
@@ -82,19 +86,38 @@ fn check_prime(n: u64) -> bool {
     }
 }
 
+fn get_max_prime(n: u64) -> u64 {
+    if n == 2 { return n; }
+
+    match n > 2 && n % 2 != 0 {
+        false => n,
+        true  => {
+            let stop = (n as f64).sqrt().floor() as u64;
+            let mut modulo = 3;
+
+            while n % modulo != 0 && modulo < stop { modulo += 2; }
+            modulo
+        }
+    }
+}
+
 fn crack(key: u64, quality: u64) -> (u64, u64) {
     let m = (key as f64).sqrt().floor() as u64;
-    let n = (m as f64).sqrt().floor() as u64;
-    
-    println!("Start filtering, cracking key: {:?}, sqrt^2: {:?}, sqrt^4: {:?}", key, m, n);
-    let mut filter = PrimeFilter::new(n, m, quality);
-    let primes = filter.filter();
-    println!("Found {:?} primes", primes.len());
-    // let primes = primes.iter().rev().map(|p| *p).collect::<Vec<u64>>();
-    for prime in primes {
-         match key % prime == 0 {
-            true => return (prime, key/prime),
-            false => ()
+    let mut n = (m as f64).sqrt().floor() as u64;
+    let batch = 10000;
+
+    println!("Range of search: <{:?}, {:?}>, total numbers: {:?}", n, m, m-n);
+    let mut filter = PrimeFilter::new(n, quality);
+
+    while n < m {
+        n += batch;
+        let primes = filter.filter(n);
+
+        for prime in primes {
+            match key % prime == 0 {
+                true => return (prime, key/prime),
+                false => ()
+            }
         }
     }
     (0,0)
@@ -103,88 +126,77 @@ fn crack(key: u64, quality: u64) -> (u64, u64) {
 fn crack_classic(key: u64) -> (u64, u64) {
     let m = (key as f64).sqrt().floor() as u64;
     let n = (m as f64).sqrt().floor() as u64;
-    
+
     println!("Start filtering, cracking key: {:?}, sqrt^2: {:?}, sqrt^4: {:?}", key, m, n);
 
-    // for prime in n..m {
-    //      match key % prime == 0 {
-    //         true => return (prime, key/prime),
-    //         false => ()
-    //     }
-    // }
-     for prime in n..m {
-         match check_prime(prime) {
-            true => return (prime, key/prime),
-            false => ()
-        }
-    }
-    (0, 0)
+    let prime = get_max_prime(key);
+    (prime, key/prime)
 }
 
 
 #[derive(Clone, Debug)]
 pub struct PrimeFilter {
     first: u64,
-    last:  u64,
     rotors: Vec<(u64, u64)>
 }
 
 impl  PrimeFilter {
-    pub fn new(first: u64, last: u64, primes_quantity: u64) -> Self {
-        let mut primes = Vec::new();
-        let (mut k, mut n) = (0, 2);
-
-        while primes_quantity > k {
-            if check_prime(n) { primes.push(n); k += 1; }
-            n += 1;
-        }
+    pub fn new(first: u64, primes_quantity: u64) -> Self {
+        let mut primes = first_n_primes(primes_quantity);
 
         let rotors = primes.iter()
             .map(|p| (*p, first % *p))
             .collect::<Vec<(_,_)>>();
 
-        PrimeFilter { first, last, rotors }
+        PrimeFilter { first, rotors }
     }
 
-    pub fn filter(&mut self) -> Vec<u64> {
+    pub fn filter(&mut self, last: u64) -> Vec<u64> {
         let mut primes = Vec::new();
         let mut actual = self.first;
 
         if actual % 2 == 0 {
-            if self.check_prime(){
-                primes.push(actual);
-            }
             self.rotate(1);
             actual += 1;
         }
 
-        while actual < self.last {
-            if self.check_prime() {
+        while actual < last {
+            if self.rotate(2) {
                 primes.push(actual);
             }
-            self.rotate(2);
             actual += 2;
         }
+
+        self.first = actual;
 
         primes
     }
 
-    fn check_prime(&mut self) -> bool {
-        for rotor in self.rotors.iter() {
-            if rotor.1 == 0 { return false; }
-        }
-
-        true
-    }
-
-    fn rotate(&mut self, steps: u64) {
+    fn rotate(&mut self, steps: u64) -> bool {
+        let mut np = true;
         let rotors = self.rotors
             .iter_mut()
-            .map(|p| Self::turn(&p, steps))
+            .map(|p| {
+                if np && p.1 == 0 { np = false; }
+                Self::turn(&p, steps)
+            })
             .collect::<Vec<(_,_)>>();
 
         self.rotors = rotors;
+
+        np
     }
+
+    // fn rotate(&mut self, steps: u64) -> bool {
+    //     let mut np = true;
+
+    //     for mut rotor in self.rotors {
+    //         if np && rotor.1 == 0 { np = false; }
+    //         *rotor = Self::turn(rotor, steps)
+    //     }
+
+    //     np
+    // }
 
     fn turn(rotor: &(u64, u64), steps: u64) -> (u64, u64) {
         match rotor.0 > rotor.1 + steps {
